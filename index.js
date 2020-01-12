0b7971a54b77af0f44c816651d1f64c243ecf16d
John Recny <jrecny@outlook.com>
2020-01-11 21:47:40 -0500

    fixed stuff

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //======================MatterJS config ===================
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 3;
-const cellsVertical = 3;
+const cellsHorizontal = 15;
+const cellsVertical = 10;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
